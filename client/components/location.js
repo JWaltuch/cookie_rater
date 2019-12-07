5c8d8f31eeaf1e77a2fb803a8c81d1baf517daf9
JWaltuch <jenna.waltuch@gmail.com>
2019-12-07 11:35:57 -0500

    changed condition for notes in location

--- client/components/location.js
+++ client/components/location.js
@@ -5,7 +5,7 @@ export const Location = props => {
   return (
     <div>
       <h2>{props.location.name}</h2>
-      {props.location.notes}
+      {props.location.notes && <div>{props.location.notes}</div>}
       <Link to={`/reviews/${props.location.id}`}>Reviews</Link>
     </div>
   )
