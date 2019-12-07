041281b7f3fc0619d5f118ebff02ff9b569f4863
JWaltuch <jenna.waltuch@gmail.com>
2019-12-07 11:16:47 -0500

    wrote reducers

--- client/store/review.js
+++ client/store/review.js
@@ -4,49 +4,60 @@ import axios from 'axios'
  * ACTION TYPES
  */
 const GOT_REVIEWS_BY_USER = 'GOT_REVIEWS_BY_USER'
-const GOT_REVIEWS_FOR_LOCATION = 'GOT_REVIEWS_FOR_LOCATION'
+const GOT_REVIEWS_BY_LOCATION = 'GOT_REVIEWS_FOR_LOCATION'
 const CREATED_REVIEW = 'CREATED_REVIEW'
 const DESTROYED_REVIEW = 'DESTROYED_REVIEW'
 
 /**
  * INITIAL STATE
  */
-const reviews = []
-
-///SHOULD USER REVIEWS LIVE IN USER OR REVIEWS????
+const reviews = {reviewsByUser: [], reviewsByLocation: []}
 
 /**
  * ACTION CREATORS
  */
-const gotLocations = newLocations => ({type: GOT_LOCATIONS, newLocations})
-const createdLocations = location => ({type: CREATED_LOCATION, location})
-const updatedLocations = location => ({type: UPDATED_LOCATION, location})
+const gotReviewsByUser = allReviews => ({type: GOT_REVIEWS_BY_USER, allReviews})
+const gotReviewsByLocation = allReviews => ({
+  type: GOT_REVIEWS_BY_LOCATION,
+  allReviews
+})
+const createdReview = review => ({type: CREATED_REVIEW, review})
+const destroyedReview = review => ({type: DESTROYED_REVIEW, review})
 
 /**
  * THUNK CREATORS
  */
-export const getLocations = () => async dispatch => {
+export const getReviewsByUser = () => async dispatch => {
   try {
-    const {data} = await axios.get('/api/locations')
-    dispatch(gotLocations(data))
+    const {data} = await axios.get('/api/users/reviews')
+    dispatch(gotReviewsByUser(data))
   } catch (err) {
     console.error(err)
   }
 }
 
-export const createLocation = locationInfo => async dispatch => {
+export const getReviewsByLoction = locId => async dispatch => {
   try {
-    const {data} = await axios.post('/api/locations', locationInfo)
-    dispatch(createdLocations(data))
+    const {data} = await axios.get(`/api/reviews/reviews/${locId}`)
+    dispatch(gotReviewsByLocation(data))
   } catch (err) {
     console.error(err)
   }
 }
 
-export const updateLocation = (locId, locationInfo) => async dispatch => {
+export const createReview = reviewBody => async dispatch => {
   try {
-    const {data} = await axios.put(`/api/locations/${locId}`, locationInfo)
-    dispatch(updatedLocations(data))
+    const {data} = await axios.post('/api/reviews', reviewBody)
+    dispatch(createdReview(data))
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+export const destroyReview = id => async dispatch => {
+  try {
+    const {data} = await axios.delete(`/api/reviews/${id}`)
+    dispatch(destroyedReview(data))
   } catch (err) {
     console.error(err)
   }
@@ -55,20 +66,33 @@ export const updateLocation = (locId, locationInfo) => async dispatch => {
 /**
  * REDUCER
  */
-export default function(state = locations, action) {
+export default function(state = reviews, action) {
   switch (action.type) {
-    case GOT_LOCATIONS:
-      return action.newLocations
-    case CREATED_LOCATION: {
-      let newLocations = [...state]
-      newLocations.push(action.location)
-      return newLocations
+    case GOT_REVIEWS_BY_USER:
+      return {...state, reviewsByUser: action.allReviews}
+    case GOT_REVIEWS_BY_LOCATION:
+      return {...state, reviewsByLocation: action.allReviews}
+    case CREATED_REVIEW: {
+      let newReviewsByUser = [...state.reviewsByUser]
+      let newReviewsByLocation = [...state.reviewsByLocation]
+      newReviewsByUser.push(action.review)
+      newReviewsByLocation.push(action.review)
+      return {
+        reviewsByUser: newReviewsByUser,
+        reviewsByLocation: newReviewsByLocation
+      }
     }
-    case UPDATED_LOCATION: {
-      let newLocations = state.locations.filter(
-        location => location.id !== action.location.id
+    case DESTROYED_REVIEW: {
+      let newReviewsByUser = state.reviewsByUser.filter(
+        review => review.id !== action.review.id
       )
-      return newLocations
+      let newReviewsByLocation = state.reviewsByLocation.filter(
+        review => review.id !== action.review.id
+      )
+      return {
+        reviewsByUser: newReviewsByUser,
+        reviewsByLocation: newReviewsByLocation
+      }
     }
     default:
       return state
