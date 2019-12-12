b37e2bf5ad6f3c6cd57b2c105333af5b11981513
JWaltuch <jenna.waltuch@gmail.com>
2019-12-11 22:32:49 -0500

    started writing users list component

---
+++ client/components/users.js
@@ -0,0 +1,58 @@
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
+//----
+import {getReviewsByLocation, getReviewsByUser} from '../store/review'
+import {getSingleLocation} from '../store/location'
+//----
+import {User} from './user'
+
+class Users extends Component {
+  componentDidMount() {
+    //get users
+  }
+
+  //display list of users
+  //display approve button for unapproved users
+  //display unapprove for other users?
+  //onclick, approve button approves users
+  //takes a req.body with a type, and a userid in params
+  render() {
+    return reviewsToRender ? (
+      <div className="page-top">
+        <h2>{title}</h2>
+        {this.props.userIsApproved &&
+          this.props.match.path !== '/reviews/me' && (
+            <Link to={`/reviews/${this.props.location.id}/add`}>
+              Add Review
+            </Link>
+          )}
+        {reviewsToRender.map(review => (
+          <Review key={review.id} review={review} />
+        ))}
+      </div>
+    ) : (
+      <div>Loading</div>
+    )
+  }
+}
+
+const mapState = (state, ownProps) => {
+  return {
+    userIsApproved:
+      state.user.type === 'approved' || state.user.type === 'admin',
+    reviewsByUser: state.review.reviewsByUser,
+    reviewsByLocation: state.review.reviewsByLocation,
+    location: state.location.singleLocation,
+    match: ownProps.match
+  }
+}
+
+const mapDispatch = dispatch => {
+  return {
+    getReviewsByUser: () => dispatch(getReviewsByUser()),
+    getReviewsByLocation: locId => dispatch(getReviewsByLocation(locId)),
+    getSingleLocation: locId => dispatch(getSingleLocation(locId))
+  }
+}
+
+export default connect(mapState, mapDispatch)(Users)
--- public/bundle.js
+++ public/bundle.js
@@ -836,14 +836,123 @@ UserHome.propTypes = {
 
 /***/ }),
 
-/***/ "./client/components/users.js":
-/*!************************************!*\
-  !*** ./client/components/users.js ***!
-  \************************************/
+/***/ "./client/components/user.js":
+/*!***********************************!*\
+  !*** ./client/components/user.js ***!
+  \***********************************/
 /*! no static exports found */
 /***/ (function(module, exports) {
 
 
+
+/***/ }),
+
+/***/ "./client/components/users.js":
+/*!************************************!*\
+  !*** ./client/components/users.js ***!
+  \************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var react_redux__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react-redux */ "./node_modules/react-redux/es/index.js");
+/* harmony import */ var _store_review__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../store/review */ "./client/store/review.js");
+/* harmony import */ var _store_location__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ../store/location */ "./client/store/location.js");
+/* harmony import */ var _user__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./user */ "./client/components/user.js");
+/* harmony import */ var _user__WEBPACK_IMPORTED_MODULE_4___default = /*#__PURE__*/__webpack_require__.n(_user__WEBPACK_IMPORTED_MODULE_4__);
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+
+ //----
+
+
+ //----
+
+
+
+var Users =
+/*#__PURE__*/
+function (_Component) {
+  _inherits(Users, _Component);
+
+  function Users() {
+    _classCallCheck(this, Users);
+
+    return _possibleConstructorReturn(this, _getPrototypeOf(Users).apply(this, arguments));
+  }
+
+  _createClass(Users, [{
+    key: "componentDidMount",
+    value: function componentDidMount() {} //get users
+    //display list of users
+    //display approve button for unapproved users
+    //display unapprove for other users?
+    //onclick, approve button approves users
+    //takes a req.body with a type, and a userid in params
+
+  }, {
+    key: "render",
+    value: function render() {
+      return reviewsToRender ? react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+        className: "page-top"
+      }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h2", null, title), this.props.userIsApproved && this.props.match.path !== '/reviews/me' && react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(Link, {
+        to: "/reviews/".concat(this.props.location.id, "/add")
+      }, "Add Review"), reviewsToRender.map(function (review) {
+        return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(Review, {
+          key: review.id,
+          review: review
+        });
+      })) : react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", null, "Loading");
+    }
+  }]);
+
+  return Users;
+}(react__WEBPACK_IMPORTED_MODULE_0__["Component"]);
+
+var mapState = function mapState(state, ownProps) {
+  return {
+    userIsApproved: state.user.type === 'approved' || state.user.type === 'admin',
+    reviewsByUser: state.review.reviewsByUser,
+    reviewsByLocation: state.review.reviewsByLocation,
+    location: state.location.singleLocation,
+    match: ownProps.match
+  };
+};
+
+var mapDispatch = function mapDispatch(dispatch) {
+  return {
+    getReviewsByUser: function getReviewsByUser() {
+      return dispatch(Object(_store_review__WEBPACK_IMPORTED_MODULE_2__["getReviewsByUser"])());
+    },
+    getReviewsByLocation: function getReviewsByLocation(locId) {
+      return dispatch(Object(_store_review__WEBPACK_IMPORTED_MODULE_2__["getReviewsByLocation"])(locId));
+    },
+    getSingleLocation: function getSingleLocation(locId) {
+      return dispatch(Object(_store_location__WEBPACK_IMPORTED_MODULE_3__["getSingleLocation"])(locId));
+    }
+  };
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (Object(react_redux__WEBPACK_IMPORTED_MODULE_1__["connect"])(mapState, mapDispatch)(Users));
 
 /***/ }),
 
@@ -917,7 +1026,6 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _components__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./components */ "./client/components/index.js");
 /* harmony import */ var _components_reviews__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ./components/reviews */ "./client/components/reviews.js");
 /* harmony import */ var _components_users__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ./components/users */ "./client/components/users.js");
-/* harmony import */ var _components_users__WEBPACK_IMPORTED_MODULE_6___default = /*#__PURE__*/__webpack_require__.n(_components_users__WEBPACK_IMPORTED_MODULE_6__);
 /* harmony import */ var _components_review_form__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(/*! ./components/review-form */ "./client/components/review-form.js");
 /* harmony import */ var _store__WEBPACK_IMPORTED_MODULE_8__ = __webpack_require__(/*! ./store */ "./client/store/index.js");
 function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
@@ -998,7 +1106,7 @@ function (_Component) {
         component: _components__WEBPACK_IMPORTED_MODULE_4__["UserHome"]
       })), isAdmin && react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react_router_dom__WEBPACK_IMPORTED_MODULE_2__["Switch"], null, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react_router_dom__WEBPACK_IMPORTED_MODULE_2__["Route"], {
         path: "/users",
-        component: _components_users__WEBPACK_IMPORTED_MODULE_6___default.a
+        component: _components_users__WEBPACK_IMPORTED_MODULE_6__["default"]
       })), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react_router_dom__WEBPACK_IMPORTED_MODULE_2__["Route"], {
         component: _components__WEBPACK_IMPORTED_MODULE_4__["Login"]
       }));
